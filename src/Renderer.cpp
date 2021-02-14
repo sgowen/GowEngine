992fc7d89b80bdc1529ad866f44211d11de1071b
Stephen Gowen <dev.sgowen@gmail.com>
2021-02-14 12:26:20 -0600

    Adding Renderer class that will contain convenience methods for common rendering tasks

---
+++ src/Renderer.cpp
@@ -0,0 +1,14 @@
+//
+//  Renderer.cpp
+//  GowEngine
+//
+//  Created by Stephen Gowen on 2/14/21.
+//  Copyright © 2021 Stephen Gowen. All rights reserved.
+//
+
+#include "Renderer.hpp"
+
+Renderer::Renderer()
+{
+    // TODO
+}
---
+++ src/Renderer.hpp
@@ -0,0 +1,19 @@
+//
+//  Renderer.hpp
+//  GowEngine
+//
+//  Created by Stephen Gowen on 2/14/21.
+//  Copyright © 2021 Stephen Gowen. All rights reserved.
+//
+
+#pragma once
+
+class Renderer
+{
+public:
+    Renderer();
+    ~Renderer() {}
+    
+private:
+    // TODO
+};
