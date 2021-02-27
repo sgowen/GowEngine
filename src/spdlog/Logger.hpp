cc9c909b42343a4a33b35612a40e00cc4ce52251
Stephen Gowen <dev.sgowen@gmail.com>
2021-02-27 17:30:19 -0600

    Adding stubs for new dep impl's

---
+++ src/spdlog/Logger.hpp
@@ -0,0 +1,9 @@
+//
+//  Logger.hpp
+//  GowEngine
+//
+//  Created by Stephen Gowen on 2/27/21.
+//  Copyright © 2021 Stephen Gowen. All rights reserved.
+//
+
+#pragma once
---
+++ src/tinythreadpp/ThreadManager.hpp
@@ -0,0 +1,9 @@
+//
+//  ThreadManager.hpp
+//  GowEngine
+//
+//  Created by Stephen Gowen on 2/27/21.
+//  Copyright © 2021 Stephen Gowen. All rights reserved.
+//
+
+#pragma once
