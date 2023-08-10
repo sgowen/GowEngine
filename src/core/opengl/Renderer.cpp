07af0354c4d1ce5bfb0c1b300d919a3a58e8e3ea
sgowen <main.sgowen@gmail.com>
2023-08-10 18:45:19 -0500

    modulus the texture width

--- src/core/opengl/Renderer.cpp
+++ src/core/opengl/Renderer.cpp
@@ -255,7 +255,9 @@ void Renderer::renderParallaxLayers(std::vector<Entity*>& layers, std::string te
         
         TextureRegion& trRaw = ASSETS_MGR.textureRegion(e->renderController()->getTextureMapping(),  e->stateTime());
         
+        Texture& t = ASSETS_MGR.texture(texture);
         uint16_t x = (m._desc._left * parallaxSpeedRatio) * gameToPixelRatio;
+        x %= t._width;
         TextureRegion tr = trRaw.copyWithX(x);
         
         spriteBatcher().addSprite(tr, e->position()._x + m._desc._left, e->position()._y, e->width(), e->height(), e->angle(), e->isXFlipped());
